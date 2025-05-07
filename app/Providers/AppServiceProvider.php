24ee2ca340d9a5e4c0b7ed3be345a31c84dffb36
Michał Krok <krokmichal1@gmail.com>
2025-05-07 19:25:14 +0200

    Fix mysql key length error  by setting utf8 charset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,11 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        Schema::defaultStringLength(191);
+
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
-        Schema::defaultStringLength(191);
+        
 
         
     }
