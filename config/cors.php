293ee84c444398120a1210fcd2f3cbb49a858623
Michał Krok <krokmichal1@gmail.com>
2025-05-19 18:36:47 +0200

    added allowed origins in cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost:5173', 'http://localhost:5174'],
+    'allowed_origins' => ['http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost:5173', 'http://localhost:5174', 'https://krokmichal.github.io'],
     // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
